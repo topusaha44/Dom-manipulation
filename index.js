a7bffe53c1a03a9f40b754bae3303cae9388d5bf
topusahajh <topusahajh94@gmail.com>
2023-08-22 18:55:52 +0600

    add all

---
+++ index.js
@@ -0,0 +1,29 @@
+// let total = 0;
+
+// function handleCLikBtn(target) {
+//   const selectedItemContainer = document.getElementById("selected-items");
+//   const itemName = target.parentNode.childNodes[1].innerText;
+//   const li = document.createElement("li");
+//   li.innerText = itemName;
+//   selectedItemContainer.appendChild(li);
+//   const price = target.parentNode.childNodes[5].innerText.split(" ")[1];
+//   total = parseInt(total) + parseInt(price);
+//   document.getElementById("total").innerText = total;
+// }
+
+// document.getElementById("").addEventListener("click", function (e) {
+//     e.target
+// })
+
+let total = 0;
+function handleCLikBtn(target) {
+  const itemName = target.parentNode.childNodes[1].innerText
+  const li = document.createElement('li')
+  li.innerText = itemName
+  const selectedItemContainer = document.getElementById('selected-items')
+  selectedItemContainer.appendChild(li)
+  const price = target.parentNode.childNodes[5].innerText.split(" ")[1];
+  total = parseInt(total) + parseInt(price);
+  document.getElementById("total").innerText = total;
+  
+}
